bc0df97004589635cf4334234c8243ede8905182
unknown <warmachine>
2021-10-27 22:07:00 +0530

    Today's Progress

--- Trees/12_Inorder_Preorder_Tree.py
+++ Trees/12_Inorder_Preorder_Tree.py
@@ -8,16 +8,16 @@ from BinaryTree import TreeNode
 def construct(preorder: list[int], inorder: list[int]) -> TreeNode | None:
     index = [0]
 
-    def build_tree(start, end):
-        if start > end: return
+    def build_tree(start, end) -> TreeNode | None:
+        if start > end: return None
         node = TreeNode(preorder[index[0]])
         index[0] += 1
         if start == end: return node
-        pos = inorder.index(node.data)
+        pos = inorder.index(node.data)  # [i for i in range(start, end + 1) if inorder[i] == node.data][0]
         node.left, node.right = build_tree(start, pos - 1), build_tree(pos + 1, end)
         return node
 
-    return build_tree(0, len(preorder) - 1)
+    return build_tree(0, len(inorder) - 1)
 
 
 def print_tree(root):
